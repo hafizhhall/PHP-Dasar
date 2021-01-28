3fbe6c84abc1e6cdd65315f97e8f601beb369629
Hafizh Athallah <hafizhhall@users.noreply.github.com>
2021-01-28 14:26:43 +0700

    Variabel Variabels

--- Variabel.php
+++ Variabel.php
@@ -7,3 +7,16 @@ echo $name;
 echo "\n";
 echo "Age: ";
 echo $age;
+echo "\n";
+
+//Variabel Variabels
+//Terlalu membingunkan
+$name = "Hafizh";
+$$name = "Keren";
+
+echo "\$name = ";
+echo $name;
+echo "\n";
+echo "\$Hafizh";
+echo $Hafizh;
+echo "\n";
